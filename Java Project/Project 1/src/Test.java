6be96edf5f3583ffea65769b04d14239b4d00244
TimLee6 <lizhao@my.swjtu.edu.cn>
2020-07-03 19:56:44 +0800

    更新1

---
+++ Java Project/Project 1/src/Test.java
@@ -0,0 +1,9 @@
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    public Test() {
+    }
+}
